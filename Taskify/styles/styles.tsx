04f3d516be39e5eaafa3dad7b66a2cc636a0053b
tomamac <mek777@hotmail.co.th>
2024-09-28 12:01:17 +0700

    update styles

---
+++ Taskify/styles/styles.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const colors = {
+  taskify100: "#16423C", //Primary Buttons, Labels (Text)
+  taskify75: "#6A9C89", //Secondary Buttons, Highlight
+  taskify50: "#C4DAD2", //Component Background color
+  taskify25: "#E9EFEC", //Background color
+};
+
+// const styles = StyleSheet.create({
+//     colors:{
+//         BackgroundColor: "#E9EFEC",
+//     },
+// });
+
+export default /* styles */ colors;
